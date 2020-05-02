d1562f65d3ba303705431092ab4c61d07fd94e23
Helena Lucia <hlucia@flashpoint-intel.com>
2020-05-01 23:17:50 -0400

    Adds healthcheck endpoint and initial unit tests

---
+++ tests/test_api.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_import():
+    """Initial test for import
+    """
+    from glittr import api
+
+@pytest.mark.skip
+def test_fails():
+    """Failing test
+    """
+    assert False
