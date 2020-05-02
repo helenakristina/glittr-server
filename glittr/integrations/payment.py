b4a2862c83611272b217c2246debd8290c6a780b
Ruth Fombrun <rfombrun@gmail.com>
2020-05-02 13:39:29 -0600

    change how we get stripe key env var to avoid KeyError

--- glittr/integrations/payment.py
+++ glittr/integrations/payment.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python3.6
 """
 This module contains the server-side code for accepting a payment with Stripe.
 """
@@ -9,7 +8,7 @@ import stripe
 from flask import Flask, render_template, jsonify, request
 from flask_restful import Resource, Api
 
-stripe.api_key = os.environ['STRIPE_API_KEY']
+stripe.api_key = os.environ.get('STRIPE_API_KEY')
 
 
 
