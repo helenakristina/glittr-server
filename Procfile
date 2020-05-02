08919f7056c04970d9b26b1caf35e425917188da
Helena Lucia <hlucia@flashpoint-intel.com>
2020-05-02 00:08:48 -0400

    Adds gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn glittr:api
---
+++ tests/test_api.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def test_import():
+    """Initial test for import
+    """
+    from glittr import api
+
+def test_passes():
+    """Failing test
+    """
+    assert True
